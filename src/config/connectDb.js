c96b784bd54acd6aad2e8c0323fcd8667356224f
raja.usamah <raja.usamah@gmail.com>
2026-07-03 03:05:21 +0500

    fixed mongodb issue

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,9 +1,36 @@
 import mongoose from "mongoose";
 import config from "./config.js";
+
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return mongoose.connection;
+  }
+
+  if (!global._mongoose) {
+    global._mongoose = { conn: null, promise: null };
+  }
+
+  if (!global._mongoose.promise) {
+    const opts = {
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+    };
+
+    global._mongoose.promise = mongoose
+      .connect(config.MONGO_URI, opts)
+      .then((mongooseInstance) => {
+        return mongooseInstance.connection;
+      })
+      .catch((error) => {
+        global._mongoose.promise = null;
+        throw error;
+      });
+  }
+
   try {
-    await mongoose.connect(config.MONGO_URI);
+    global._mongoose.conn = await global._mongoose.promise;
     console.log("MongoDB Connected Successfully");
+    return global._mongoose.conn;
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
     process.exit(1);
