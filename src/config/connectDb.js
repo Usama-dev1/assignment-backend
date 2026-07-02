8972e7be6e1539b5c96b1e31554aa95250ee4a86
raja.usamah <raja.usamah@gmail.com>
2026-07-03 03:32:29 +0500

    rollback mongo issue

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,36 +1,9 @@
 import mongoose from "mongoose";
 import config from "./config.js";
-
 const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return mongoose.connection;
-  }
-
-  if (!global._mongoose) {
-    global._mongoose = { conn: null, promise: null };
-  }
-
-  if (!global._mongoose.promise) {
-    const opts = {
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-    };
-
-    global._mongoose.promise = mongoose
-      .connect(config.MONGO_URI, opts)
-      .then((mongooseInstance) => {
-        return mongooseInstance.connection;
-      })
-      .catch((error) => {
-        global._mongoose.promise = null;
-        throw error;
-      });
-  }
-
   try {
-    global._mongoose.conn = await global._mongoose.promise;
+    await mongoose.connect(config.MONGO_URI);
     console.log("MongoDB Connected Successfully");
-    return global._mongoose.conn;
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
     process.exit(1);
